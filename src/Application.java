8e97f641eee35f50e368e8b81d0647272aca58a8
Omolewa Samuel <samomolewa.com@gmail.com>
2021-03-09 13:20:23 -0800

    Tutorial10

---
+++ src/Application.java
@@ -0,0 +1,73 @@
+public class Application {
+	public static void main(String[] args) {
+		
+		/*
+		int day = 10;
+		*/
+		
+		/*
+		if(day == 1) {
+			System.out.println("Sunday");
+		}else if(day == 2)  {
+			System.out.println("Monday");
+		}else if(day == 3) {
+			System.out.println("Tuesday");
+		}else if(day == 4) {
+			System.out.println("Wednesday");
+		}else if(day == 5) {
+			System.out.println("Thursady");
+		}else if(day == 6) {
+			System.out.println("Friday");
+		}else if(day == 7) {
+			System.out.println("Saturday");
+		}
+		*/
+		 
+		/*
+		switch(day) {
+			case 1:
+				System.out.println("Sunday");
+					break;
+			case 2:
+				System.out.println("Monday");
+					break;
+			case 3:
+				System.out.println("Tuesday");
+					break;
+			case 4:
+				System.out.println("Wednesday");
+					break;
+			case 5:
+				System.out.println("Thursday");
+					break;
+			case 6:
+				System.out.println("Friday");
+					break;
+			case 7:
+				System.out.println("Saturday");
+					break;
+					default :
+						System.out.println("not a valid day number");
+		}
+		*/
+		
+		String dog = "German Shepherd";
+		
+		switch(dog) {
+			case "German Shepherd":
+				System.out.println("small dog");
+				break;
+			
+			case "Rotweiler":
+				System.out.println("big dog");
+				break;
+				
+				default :
+					System.out.println("try a different dog name");
+			
+		}
+		
+		
+	}
+
+}
